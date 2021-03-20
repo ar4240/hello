fdfa2cd0c7dfcd9cb1d0c21836233409281cbdf2
Adithyya Vamsi <cskadi1999@gmail.com>
2021-03-20 22:02:32 +0530

    c8

--- world.c
+++ world.c
@@ -1,9 +1,15 @@
-#include<stdio.h>
 /**
+ * @file world.c
+ * @author your name (you@domain.com)
  * @brief 
+ * @version 0.1
+ * @date 2021-03-20
  * 
- * @return int 
+ * @copyright Copyright (c) 2021
+ * 
  */
+#include<stdio.h>
+
 int main()
 {
     printf("Hello World");
