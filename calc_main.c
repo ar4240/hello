30870c8b22ecabbb35e05c66659c5dbdab06d902
Adithyya Vamsi <cskadi1999@gmail.com>
2021-03-24 12:19:25 +0530

    done

--- calc_main.c
+++ calc_main.c
@@ -3,6 +3,12 @@
 
 int main()
 {
-    test_main();
+    int a=10,b=10;
+    printf("%d\n",sum(a,b));
+    printf("%d\n",sub(a,b));
+    printf("%d\n",mul(a,b));
+    printf("%d\n",div(a,b));
+    
+    //test_main();
 	return 0;
 }
--- inc/calc_head.h
+++ inc/calc_head.h
@@ -1,10 +1,10 @@
 
-#ifndef __calc.h__
+#ifndef __calc_h__
 
-#define __calc.h__
+#define __calc_h__
 
 int sum(int num1,int num2);
-int sub(int num1, int num2);
+int sub(int num1,int num2);
 int div(int num1,int num2);
 int mul(int num1,int num2);
 
