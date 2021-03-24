e1666ef5b69e6be6261ef49d32f4e64c4db3de93
Adithyya Vamsi <cskadi1999@gmail.com>
2021-03-24 12:20:46 +0530

    final

--- inc/calc_head.h
+++ inc/calc_head.h
@@ -1,4 +1,13 @@
-
+/**
+ * @file calc_head.h
+ * @author Adithya & Shubham (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-03-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef __calc_h__
 
 #define __calc_h__
